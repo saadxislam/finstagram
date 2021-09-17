467cb6426e86db69b8ab501dd28f809f809db0cf
saadxislam <muhammadsaad1010@gmail.com>
2021-09-16 21:43:18 -0700

    added global styling for homepage

--- finstagram/pages/_app.js
+++ finstagram/pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+// add bootstrap css
+import "bootstrap/dist/css/bootstrap.css";
+import Head from "next/head";
+import React from "react";
+import PropTypes from "prop-types";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
